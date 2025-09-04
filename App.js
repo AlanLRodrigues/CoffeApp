846ceba6cd649454719788d708238dfcc00023b7
Alan Rodrigues <alanrodrigues70@gmail.com>
2025-09-04 10:39:39 -0300

    Replace wrong files with the correct version

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity ,Button, Image ,ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // Deixa seu app preparado para respeitar áreas seguras da tela (tipo o lugar da câmera e bateria)
+    <SafeAreaProvider>
+      // Área segura para o conteúdo não ficar atrás do notch, barra de status ou botões do celular
+      <SafeAreaView style={styles.container}  edges={['top', 'bottom', 'left', 'right']}>
+        <ImageBackground
+          source={require('./assets/background.jpg')} 
+          style={styles.ImageBackground}
+          resizeMode='cover'
+        >
+
+        <Image
+          source={require('./assets/cafe.png')} 
+          style={styles.cafeImage}
+        />
+
+        <Text style={styles.title}>
+          Coffee is the{'\n'}{/* Quebra de linha no Native */}
+          Best thing in{'\n'}
+          The world
+        </Text>
+
+        <TouchableOpacity style={styles.botao} onPress={() => alert('Botão pressionado')}>
+          <Text style={styles.textoBotao}>Faça seu pedido</Text>
+        </TouchableOpacity>
+
+        <StatusBar style="dark"/>
+
+        </ImageBackground>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#000',
+  },
+  ImageBackground: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+    cafeImage: {
+    width: 200,
+    height: 200,
+    marginBottom: 10,
+  },
+    title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 10,
+  },
+    botao: {
+    width: 170,
+    height: 50,
+    justifyContent: 'center', // alinha o texto verticalmente
+    alignItems: 'center',     // alinha o texto horizontalmente
+    backgroundColor: '#2ECC71',
+    borderRadius: 25,
+    marginTop: 20,
+  },
+    textoBotao: {
+    color: '#fff',
+    fontSize: 15,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
